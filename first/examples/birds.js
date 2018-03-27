42b54b73fcd29ebc98b357b5429e92393f42773c
aStutzS4 <astutzman.s4@gmail.com>
2018-03-27 15:37:16 -0400

    Added Birds file for using Routes in another file

---
+++ first/examples/birds.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var router = express.Router()
+
+router.use(function(req, res, next){
+  console.log('Birds Middleware')
+  next()
+})
+
+//Define a home page route
+router.get('/', (req, res) => {
+    res.send('Birds home page')
+})
+
+//define the about router
+router.get('/about', (req, res) => {
+  res.send('About birds')
+})
+
+module.exports = router;

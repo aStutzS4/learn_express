620cb158b8c23c0b2a444a865ed6c578b20a67a3
aStutzS4 <astutzman.s4@gmail.com>
2018-03-27 14:04:04 -0400

    Added helloworld.js with basic routing

--- first/examples/helloworld.js
+++ first/examples/helloworld.js
@@ -5,4 +5,10 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/', (req, res) => res.send('Got a POST request'))
+
+app.put('/user', (req, res) => res.send('Got a PUT request at /user'))
+
+app.delete('/user', (req, res) => res.send('Got a DELETE request at /user'))
+
 app.listen(1337, () => console.log('Example app listening on port 3000!'))
